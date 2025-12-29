fa40f5320c3ea95491275fc8dd02de6b7a7d2323
saadArfin <saad.arfin.ug21@nsut.ac.in>
2025-12-30 01:48:10 +0530

    trpc-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
-import prisma from "@/lib/db";
+import {trpc, getQueryClient} from "@/trpc/server";
+import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
+import { Suspense } from "react";
+import { Client } from "./client";
 
-const Page = async () => {
-  const users = await prisma.user.findMany();
+const Page = () => {
+  const queryClient = getQueryClient();
+
+  void queryClient.prefetchQuery(
+    trpc.getUsers.queryOptions()
+  );
 
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
-      {JSON.stringify(users)}
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<p>loading...</p>}>
+          <Client />
+        </Suspense>
+      </HydrationBoundary>
     </div>
   );
 };
