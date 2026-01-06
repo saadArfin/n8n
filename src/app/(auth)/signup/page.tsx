53239940db9c16f0666d7f5840fdb9d82f73a83b
saadArfin <saad.arfin.ug21@nsut.ac.in>
2026-01-07 01:07:33 +0530

    implement authentication with better-auth and create user, session, account, and verification models

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,12 @@
+import RegisterForm from '@/features/auth/components/register-form'
+
+
+const Page = () => {
+  return (
+    <div>
+      <RegisterForm/>
+    </div>
+  )
+}
+
+export default Page
---
+++ src/features/auth/components/register-form.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RegisterForm = () => {
+  return (
+    <div>
+      Register 
+    </div>
+  )
+}
+
+export default RegisterForm
