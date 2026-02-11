a719df372345026451d20f1f5891ea7b1ff9e713
saadArfin <saad.arfin.ug21@nsut.ac.in>
2026-02-12 01:00:26 +0530

    Created the trpc procedure for workflows CRUD and implemented prefetching for faster rendering.

--- src/app/(dashboard)/(rest)/workflows/page.tsx
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -1,7 +1,24 @@
+import { WorkflowsList } from "@/features/workflows/components/workflows";
+import { prefetchWorkflows } from "@/features/workflows/server/prefetch";
 import { requireAuth } from "@/lib/auth-utils"
+import { HydrateClient } from "@/trpc/server";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 const Page = async () => {
-    await requireAuth()
-    return <p>workflows</p>
-}
-export default Page
+  await requireAuth();
+
+  prefetchWorkflows();
+
+  return (
+    <HydrateClient>
+      <ErrorBoundary fallback={<p>Error!</p>}>
+        <Suspense fallback={<p>Loading...</p>}>
+          <WorkflowsList />
+        </Suspense>
+      </ErrorBoundary>
+    </HydrateClient>
+  );
+};
+
+export default Page;
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -2,30 +2,9 @@
 import prisma from '@/lib/db';
 import { baseProcedure, createTRPCRouter, premiumProcedure, protectedProcedure } from '../init';
 import { inngest } from '@/inngest/client';
+import { workflowsRouter } from '@/features/workflows/server/router';
 export const appRouter = createTRPCRouter({
-
-  testAi: premiumProcedure.mutation(async () => {
-    await inngest.send({
-      name: "execute/ai",
-    })
-
-    return { success: true, message: 'Job Queued Successfully!' };
-  }),
-
-  getWorkflows: protectedProcedure.query(({ ctx }) => {
-    return prisma.workflow.findMany();
-  }),
-
-  createWorkflow: protectedProcedure.mutation(async () => {
-    await inngest.send({
-      name: "test/hello.world",
-      data: {
-        email: "saadarfin@example.com",
-      },
-    });
-
-    return { success: true, message: 'Job Queued Successfully!' };
-  })
+  workflows: workflowsRouter
 });
 
 // export type definition of API
