7e934067b57da3eaeb4d1dedbee7d66fbf797004
saadArfin <saad.arfin.ug21@nsut.ac.in>
2026-02-22 01:52:15 +0530

    implemented workflows search; leveraging custom use-entity-search hook

--- src/features/workflows/components/workflows.tsx
+++ src/features/workflows/components/workflows.tsx
@@ -1,12 +1,27 @@
 "use client";
 
-import { EntityContainer, EntityHeader } from "@/components/entity-components";
+import { EntityContainer, EntityHeader, EntitySearch } from "@/components/entity-components";
 import {
   useCreateWorkflow,
   useSuspenseWorkflows,
 } from "../hooks/use-workflows";
 import { useUpgradeModal } from "@/hooks/use-upgrade-modal";
 import { useRouter } from "next/navigation";
+import { useWorkflowsParams } from "../hooks/use-workflows-params";
+import { useEntitySearch } from "@/hooks/use-entity-search";
+
+export const WorkflowsSearch = () => {
+
+  const [params, setParams] = useWorkflowsParams()
+  const { searchValue, onSearchChange } = useEntitySearch({ params, setParams })
+  return (
+    <EntitySearch
+      value={searchValue}
+      onChange={onSearchChange}
+      placeholder="Search workflows"
+    />
+  );
+};
 
 export const WorkflowsList = () => {
   const workflows = useSuspenseWorkflows();
@@ -58,7 +73,7 @@ export const WorkflowsContainer = ({
   return (
     <EntityContainer
       header={<WorkflowsHeader />}
-      search={<></>}
+      search={<WorkflowsSearch />}
       pagination={<></>}
     >
       {children}
